e7f7f782707eb6ae7fb38f3243109d91208cd864
Siddhesh101 <sbomble58@gmail.com>
2024-03-24 15:22:00 +0530

    some changes

--- main.py
+++ main.py
@@ -12,14 +12,7 @@ def get_value(key):
         # Send the file to the client
         return send_file(file_path, as_attachment=True)
     else:
-        return jsonify({"error": "File not found"}), 404
-    """if os.path.isfile(f'NLP/{key}.txt'):  # Check if file exists
-        with open(f'NLP/{key}.txt', 'r') as file:
-            file_contents = file.readlines()  # Read lines of the file
-        return jsonify(''.join(file_contents))  # Return the list of lines as JSON
-    else:
-        return jsonify("5")"""
-
+        return jsonify({"error": "Your Module not found"}), 404
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
 
--- test_file.py
+++ test_file.py
@@ -1,7 +1,5 @@
 import requests
-
 response = requests.get("http://127.0.0.1:5000/1B")
+open('downloaded_file.py', 'wb').write(response.content)
 
-open('downloaded_file.txt', 'wb') .write(response.content)
-print("File downloaded successfully.")
 
