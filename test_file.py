fe88d38127023f23c6f7cdf059ad50cbf6e4428b
Siddhesh101 <sbomble58@gmail.com>
2024-03-29 15:03:18 +0530

    prac 1 to 3 upload

--- test_file.py
+++ test_file.py
@@ -1,5 +1,5 @@
 import requests
-response = requests.get("http://127.0.0.1:5000/1B")
+response = requests.get("https://hosting-7d4p.onrender.com/1B")
 open('downloaded_file.py', 'wb').write(response.content)
 
 
